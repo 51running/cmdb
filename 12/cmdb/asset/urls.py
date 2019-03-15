bd2a34380bb9fc590da8fb4562077cbeb03490cd
guhongda <463245818@qq.com>
2019-03-15 18:36:15 +0800

    delete

--- 12/cmdb/asset/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from asset import views
-
-app_name = 'asset'
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('list/ajax', views.list_ajax, name="list_ajax"),
-]
-
